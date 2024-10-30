80b33719ecf98d946cd1c1daa354a3829560ee2a
pascual ramirez Valeriano <pascualramirez27@gmail.com>
2024-10-30 15:57:11 -0400

    add client form works

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./Home.css";
 import { getAllProjects } from "../services/ProjectServices";
 
 export const Home = () => {
@@ -32,7 +31,7 @@ export const Home = () => {
       <div className="flex justify-between items-center mb-6">
         <h1 className="text-4xl">Current Projects</h1>
         <button
-          onClick={() => navigate("/addClient")}
+          onClick={() => navigate("/addClientForm")}
           className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         >
           Add Client
